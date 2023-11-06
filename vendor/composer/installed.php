80cb479ae415fec468773668f45e3d23172c21bf
KaiNaumann <121484675+KaiNaumann@users.noreply.github.com>
2023-11-06 09:27:27 +0100

    x

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-CT',
+    'version' => 'dev-CT',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c7c4e237b39268fd99bd5e822c5d7098ee9676a7',
+    'name' => 'fegab/newchurchtoolplugin',
+  ),
+  'versions' => 
+  array (
+    'fegab/newchurchtoolplugin' => 
+    array (
+      'pretty_version' => 'dev-CT',
+      'version' => 'dev-CT',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c7c4e237b39268fd99bd5e822c5d7098ee9676a7',
+    ),
+  ),
+);
